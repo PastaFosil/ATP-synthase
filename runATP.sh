f49c9fcbc087241cf45c80627d9cafdd6150527d
juancho <juancrlos6@gmail..com>
2023-09-07 19:28:56 -0700

    Removing executables

--- runATP.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=2:00:00
-#SBATCH --account=def-dsivak
-#SBATCH --ntasks=10               # number of MPI processes
-#SBATCH --mem-per-cpu=1G      # memory; default unit is megabytes
-module load python 
-module load scipy-stack
-python3 "/home/juanch0/projects/def-dsivak/juanch0/ATP_ratio/main.py" 8.0
